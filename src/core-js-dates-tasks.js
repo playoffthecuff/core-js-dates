6a777b935d7ceb464680507b8e1cad5d178e1d13
playoffthecuff <jmobile@bk.ru>
2024-03-22 08:30:04 +0300

    feat: 13th task solved

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -224,8 +224,12 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const dateCopy = date;
+  while (!(dateCopy.getDay() === 5 && dateCopy.getDate() === 13)) {
+    date.setDate(dateCopy.getDate() + 1);
+  }
+  return dateCopy;
 }
 
 /**
@@ -239,8 +243,17 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  if (date.getMonth() < 4) {
+    return 1;
+  }
+  if (date.getMonth() < 7) {
+    return 2;
+  }
+  if (date.getMonth() < 10) {
+    return 3;
+  }
+  return 4;
 }
 
 /**
@@ -261,8 +274,29 @@ function getQuarter(/* date */) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const result = [];
+  function parse(dateStr) {
+    const year = dateStr.slice(-4);
+    const month = +dateStr.slice(3, 5) - 1;
+    const day = dateStr.slice(0, 2);
+    return [year, month, day];
+  }
+  const start = new Date(...parse(period.start));
+  const end = new Date(...parse(period.end));
+  function format(date) {
+    return `${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear()}`;
+  }
+  while (start <= end) {
+    for (let i = 0; i < countWorkDays; i += 1) {
+      if (start <= end) result.push(format(start));
+      start.setDate(start.getDate() + 1);
+    }
+    for (let i = 0; i < countOffDays; i += 1) {
+      start.setDate(start.getDate() + 1);
+    }
+  }
+  return result;
 }
 
 /**
